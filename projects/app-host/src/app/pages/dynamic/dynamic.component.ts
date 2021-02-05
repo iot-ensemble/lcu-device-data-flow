5c1014297c2f04c7288633e300c8ee8695847e57
mcgear <michael.gearhardt@gmail.com>
2021-02-05 00:45:19 -0700

    Refactor the state into the manage.component

--- projects/app-host/src/app/pages/dynamic/dynamic.component.ts
+++ projects/app-host/src/app/pages/dynamic/dynamic.component.ts
@@ -43,16 +43,12 @@ export class DynamicComponent implements OnInit {
 
   public SideNavOpenCloseEvent: boolean;
 
-  public State: IoTEnsembleState;
-
   //  Constructors
   constructor(
-    protected iotEnsCtxt: IoTEnsembleStateContext,
     protected breakpointUtils: BreakpointUtils,
     public AnimationSrvc: AnimationService,
     protected mediaObserver: MediaObserver
   ) {
-    this.State = {};
   }
 
   //  Life Cycle
@@ -60,8 +56,6 @@ export class DynamicComponent implements OnInit {
     // this.breakpointUtils.SetupIsMobileObserver((result) =>
     //   this.handleMobileObserver(result)
     // );
-
-    this.setupStateHandler();
 
     this.mediaSubscription = this.mediaObserver.asObservable()
     .pipe(
@@ -97,41 +91,6 @@ export class DynamicComponent implements OnInit {
   }
 
   //  API Methods
-  public ColdQuery() {
-    this.State.Loading = true;
-
-    this.iotEnsCtxt.ColdQuery();
-  }
-
-  public EnrollDevice(device: IoTEnsembleDeviceEnrollment) {
-    this.State.DevicesConfig.Loading = true;
-
-    this.iotEnsCtxt.EnrollDevice(device);
-  }
-
-  public HandleTelemetryPageEvent(event: any) {
-    if (event.pageIndex + 1 !== this.State.Telemetry.Page) {
-      this.UpdateTelemetryPage(event.pageIndex + 1);
-    } else if (event.pageSize !== this.State.Telemetry.PageSize) {
-      this.UpdateTelemetryPageSize(event.pageSize);
-    }
-  }
-
-  public HandleDevicePageEvent(event: any) {
-    if (event.pageIndex + 1 !== this.State.DevicesConfig.Page) {
-      this.UpdateDeviceTablePageIndex(event.pageIndex + 1);
-    } else if (event.pageSize !== this.State.DevicesConfig.PageSize) {
-      this.UpdateDeviceTablePageSize(event.pageSize);
-    }
-  }
-
-  public IssueDeviceSASToken(deviceName: string) {
-    
-    this.State.DevicesConfig.Loading = true;
-
-    //  TODO:  Pass through expiry time in some way?
-    this.iotEnsCtxt.IssueDeviceSASToken(deviceName, 0);
-  }
 
   /**
    *
@@ -152,33 +111,6 @@ export class DynamicComponent implements OnInit {
     }
 
     return this.AnimationSrvc.IsOpen ? 'open' : 'close';
-  }
-
-  public Refresh(ctxt: string) {
-    const loadingCtxt = this.State[ctxt] || this.State;
-
-    loadingCtxt.Loading = true;
-
-    this.iotEnsCtxt.$Refresh();
-  }
-
-  public RegenerateAPIKey(keyName: string) {
-    // this.State.Loading = true;
-
-    alert('Implement regenerate: ' + keyName);
-    // this.iotEnsCtxt.RegenerateAPIKey(keyName);
-  }
-
-  public RevokeDeviceEnrollment(deviceId: string) {
-    this.State.DevicesConfig.Loading = true;
-
-    this.iotEnsCtxt.RevokeDeviceEnrollment(deviceId);
-  }
-
-  public SendDeviceMessage(payload: IoTEnsembleTelemetryPayload) {
-    this.State.Telemetry.Loading = true;
-
-    this.iotEnsCtxt.SendDeviceMessage(payload.DeviceID, payload);
   }
 
   public ToggleSideNav(): void {
@@ -206,100 +138,6 @@ export class DynamicComponent implements OnInit {
     // this.ExpandVerticalEvent = evt.fromState === 'down' ? true : false;
   }
 
-  public TelemetryDownload(query: ColdQueryModel) {
-    console.log('ColdQueryModelCall: ', query);
-
-    if (!query.Zip) {
-      this.iotEnsCtxt
-        .ColdQuery(
-          query.StartDate,
-          query.EndDate,
-          query.PageSize,
-          query.PageSize,
-          query.SelectedDeviceIds,
-          query.IncludeEmulated,
-          query.DataType,
-          query.ResultType,
-          query.Flatten,
-          query.Zip
-        )
-        .then((obs: any) => {
-          console.log('OBS: ', obs);
-          const blob = new Blob([JSON.stringify(obs.body)], {
-            type: 'text/json',
-          });
-          const url = window.URL.createObjectURL(blob);
-
-          const link = document.createElement('a');
-          link.download = 'telemetry.json';
-          link.href = url;
-          link.click();
-        });
-    }
-  }
-
-  public ToggleTelemetryEnabled() {
-    this.State.Telemetry.Loading = true;
-
-    this.iotEnsCtxt.ToggleTelemetrySync();
-  }
-
-  public ToggleEmulatedEnabled() {
-    this.State.Emulated.Loading = true;
-
-    this.iotEnsCtxt.ToggleEmulatedEnabled();
-  }
-
-  public UpdateDeviceTablePageSize(pageSize: number) {
-    this.State.DevicesConfig.Loading = true;
-
-    this.iotEnsCtxt.UpdateConnectedDevicesSync(this.State.DevicesConfig.Page, pageSize);
-  }
-
-  public UpdateDeviceTablePageIndex(page: number) {
-    this.State.DevicesConfig.Loading = true;
-
-    this.iotEnsCtxt.UpdateConnectedDevicesSync(page, this.State.DevicesConfig.PageSize);
-  }
-
-  public UpdateTelemetryPage(page: number) {
-    // console.log("calling update page: ", page)
-    this.State.Telemetry.Loading = true;
-
-    this.iotEnsCtxt.UpdateTelemetrySync(
-      this.State.Telemetry.RefreshRate,
-      page,
-      this.State.Telemetry.PageSize
-    );
-  }
-
-  public UpdateTelemetryPageSize(pageSize: number) {
-    // console.log("calling update pageSize: ", pageSize)
-    this.State.Telemetry.Loading = true;
-
-    this.iotEnsCtxt.UpdateTelemetrySync(
-      this.State.Telemetry.RefreshRate,
-      this.State.Telemetry.Page,
-      pageSize
-    );
-  }
-
-  public UpdateRefreshRate(refreshRate: number) {
-    this.State.Telemetry.Loading = true;
-
-    this.iotEnsCtxt.UpdateTelemetrySync(
-      30,
-      this.State.Telemetry.Page,
-      this.State.Telemetry.PageSize
-    );
-  }
-
-  public WarmQuery() {
-    this.State.Loading = true;
-
-    this.iotEnsCtxt.WarmQuery(null, null, null, null, null, true);
-  }
-
   //  Helpers
 
   //TODO: move this to Ref Arch - shannon
@@ -309,17 +147,5 @@ export class DynamicComponent implements OnInit {
     // if (!this.IsMobile && this.NavDrawer) {
     //   this.NavDrawer.open();
     // }
-  }
-
-  protected handleStateChanged() {
-    console.log(this.State);
-  }
-
-  protected setupStateHandler() {
-    this.iotEnsCtxt.Context.subscribe((state) => {
-      this.State = Object.assign(this.State, state);
-
-      this.handleStateChanged();
-    });
   }
 }
