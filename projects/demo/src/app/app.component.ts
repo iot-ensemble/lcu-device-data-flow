c74a57b44f62ec31a7949dbfd1ef35b9ef1957fa
mcgear <michael.gearhardt@gmail.com>
2021-02-10 09:33:30 -0700

    rxjs ref

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
 import { ThemeColorPickerService } from '@lcu/common';
 
 @Component({
