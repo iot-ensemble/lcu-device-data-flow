e08f799d3fa1cb43a4accc457f75b5d015843995
mcgear <michael.gearhardt@gmail.com>
2021-02-10 17:01:38 -0700

    Adjustments

---
+++ projects/demo/src/app/controls/manage/manage.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lcu-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss']
+})
+export class ManageComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit(): void { }
+
+}
